693ab9b6b20c4cb514c5d3c245a0fdfc9c48af4e
huzaifah256 <ssekayubauzfayunus@gmail.com>
2025-07-25 12:12:10 +0300

    Program.cs

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -2,12 +2,14 @@ using System;
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		Console.WriteLine("Hello World! This is the Exercise4 Project.");
-		Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-		List<int> numbers = new List<int>();
-	}
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+
+        List<int> numbers = new List<int>();
+
         int input;
 
         do
@@ -36,3 +38,4 @@ class Program
         Console.WriteLine($"The average is: {average}");
         Console.WriteLine($"The largest number is: {max}");
     }
+}
